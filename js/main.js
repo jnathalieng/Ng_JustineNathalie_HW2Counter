ea17ec9aacb5ed633de618940f72ddee59c52748
Justine Nathalie Ng <j_ng228769@fanshaweonline.ca>
2026-03-13 02:58:29 -0400

    Update main.js
    In this commit update main.js file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { Counter } from './counter.js';
+
+const counter1 = new Counter("#counter-container1", 10);
+const counter2 = new Counter('#counter-container2');
+const counter3 = new Counter ('#counter-container3');
+ 
+
