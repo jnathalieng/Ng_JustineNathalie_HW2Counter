19a51fc1622c6c2545dbb4583d1676219c44e5db
Justine Nathalie Ng <j_ng228769@fanshaweonline.ca>
2026-03-20 01:19:08 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,4 +6,3 @@ const counter2 = new Counter("#counter-container2", 10);
 const counter3 = new StepCounter ("#counter-container3", 100, 100);
 const counter4 = new StepCounter ("#counter-container3", 1000, 1000);
  
-
